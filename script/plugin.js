6d771cab6a5df83156e3ecf470e4848b0adaa2c8
Tim Collins <tim@timcollins.ie>
2014-09-16 11:35:51 +0100

    Wrapped greenify function in an immediately-invoked function expression.

--- script/plugin.js
+++ script/plugin.js
@@ -10,15 +10,27 @@ $(document).ready(function () {
     });
 });
 
-// Turn all links green when clicked.
-$.fn.greenify = function () {
-    // Can pass colour names as well as HTML colour codes.
-    //this.css("color", "green");
-    this.css("color", "#00FF00");
+// Wrap the greenify function in an immediately-invoked function expression
+(function($) {
+    // Turn all links green when clicked.
+    $.fn.greenify = function () {
+        // Can pass colour names as well as HTML colour codes.
+        //this.css("color", "green");
+        this.css("color", "#00FF00");
 
-    // Make the function chainable.
-    return this;
-};
+        // Make the function chainable.
+        return this;
+    };
+
+    $.ltrim = function(str) {
+        return str.replace(/^\str+/, "");
+    };
+
+    $.rtrim = function(str) {
+        return str.replace(/\s+$/, "");
+    };
+}(jQuery));
+
 
 // Example of immediately-invoked function expression.
 (function () {
