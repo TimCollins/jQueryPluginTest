7b3145b960d399117f41fb0af955e5782ad7f02f
Tim Collins <tim@timcollins.ie>
2014-09-18 11:20:56 +0100

    Refactored QUnit tests.

--- script/qunit_test.js
+++ script/qunit_test.js
@@ -1,14 +1,19 @@
 doTests = function() {
     QUnit.test("prettyDate basics", function (assert) {
-        var now = "2014-09-17T22:24:17Z";
+        
+        function testDate(then, expected) {
+            var now = "2014-09-17T22:24:17Z";
+            assert.equal(prettyDate(now, then), expected);
 
-        assert.equal(prettyDate(now, "2014-09-17T22:24:16Z"), "just now");
-        assert.equal(prettyDate(now, "2014-09-17T22:23:17Z"), "1 minute ago");
-        assert.equal(prettyDate(now, "2014-09-17T21:24:17Z"), "1 hour ago");
-        assert.equal(prettyDate(now, "2014-09-16T21:24:17Z"), "Yesterday");
-        assert.equal(prettyDate(now, "2014-09-15T21:24:17Z"), "2 days ago");
-        assert.equal(prettyDate(now, "2014-09-12T21:24:17Z"), "5 days ago");
-        assert.equal(prettyDate(now, "2010-09-12T21:24:17Z"), undefined);
+        }
+
+        testDate("2014-09-17T22:24:16Z", "just now");
+        testDate("2014-09-17T22:23:17Z", "1 minute ago");
+        testDate("2014-09-17T21:24:17Z", "1 hour ago");
+        testDate("2014-09-16T21:24:17Z", "Yesterday");
+        testDate("2014-09-15T21:24:17Z", "2 days ago");
+        testDate("2014-09-12T21:24:17Z", "5 days ago");
+        testDate("2010-09-12T21:24:17Z", undefined);
     });
 };
 
