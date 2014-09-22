34cb90882e1a0f0b25eb98de66761e59f0ff1a61
Tim Collins <tim@timcollins.ie>
2014-09-22 16:21:35 +0100

    Added a failing test to check querystring parameters.

--- script/qunit_tests.js
+++ script/qunit_tests.js
@@ -2,5 +2,9 @@ doTests = function () {
     QUnit.test("A Test", function (assert) {
         assert.equal(1, 1);
     });
+
+    QUnit.test("A Failing Test", function (assert) {
+        assert.equal(1, 2);
+    });
 };
 
